0d4205a216332f15d1ad0b297bb14dcbd91eb681
xSebix10 <seba.kontakt@wp.pl>
2025-11-08 15:02:14 +0100

    Some fixes

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 REM Nazwa pliku wynikowego
 set OUT=main.exe
 
-REM Kompilacja wszystkich cpp w katalogu
+REM Kompilacja wszystkich cpp w katalogu 
 g++ -Wall -Wextra -std=c++17 *.cpp -o %OUT%
 if ERRORLEVEL 1 (
     pause
