05fc59526f7888973a6f24ecdb26aaadeeaac692
dmichura <dejwidcode@gmail.com>
2025-11-08 14:26:32 +0100

    Create methods for Menu class

---
+++ Menu.cpp
@@ -0,0 +1,50 @@
+#include "Menu.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+// community function to clear screen console
+// https://medium.com/@ryan_forrester_/c-screen-clearing-how-to-guide-cff5bf764ccd
+void clearScreenANSI() {
+    cout << "\033[2J\033[1;1H";
+}
+
+void o1()
+{
+    cout << "opcja1\n";
+}
+
+Menu::Menu() {
+    cout << "Drzewo BST\n";
+    // klasa bst
+    this->list.push_back( {"dodawanie", o1} );
+    this->list.push_back( {"odejmowanie", o1} );
+}
+
+Menu::~Menu() {}
+
+void Menu::run() {
+    while (true)
+    {
+        clearScreenANSI();
+        cout << "\n==========================\n";
+        for (size_t i = 0; i < this->list.size(); i++)
+        {
+            cout << i+1 << ". " << this->list[i].first << "\n";
+        }
+        cout << "==========================\n";
+
+        int choose;
+        cout << "  >> ";
+        cin >> choose;
+        cout << "\nWybrales: " << choose << "";
+
+        if (choose < 1 || choose > (int) this->list.size())
+        {
+            cout << "Wybierz poprawną opcję!\n";
+            continue;
+        }
+    }
+
+}
