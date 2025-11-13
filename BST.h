ca612fbbbbe7d5fed6c153fab01023990414cba0
Dominik <dominikjonik6@gmail.com>
2025-11-13 23:15:26 +0100

    Create struct Node

---
+++ BST.h
@@ -0,0 +1,14 @@
+#ifndef BST_H
+#define BST_H
+
+#include <string>
+#include <fstream>
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+#endif
