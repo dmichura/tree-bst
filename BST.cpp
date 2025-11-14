661665b065f8ed03bdfdf3443dfc884240b05095
Dominik <dominikjonik6@gmail.com>
2025-11-14 20:54:09 +0100

    Implement recursive remove & findMin

--- BST.cpp
+++ BST.cpp
@@ -23,4 +23,64 @@ void BST::clear() {
     root = nullptr;
 }
 
+void BST::remove(int value) {
+    if (root == nullptr) {
+        std::cout << "Drzewo jest puste. Nie mozna usunac.\n";
+        return;
+    }
+    root = removeRecursive(root, value);
+}
+
+
+Node* BST::removeRecursive(Node* node, int value) {
+
+    if (node == nullptr) {
+        std::cout << "Element " << value << " nie istnieje w drzewie.\n";
+        return nullptr;
+    }
+
+
+    if (value < node->data) {
+        node->left = removeRecursive(node->left, value);
+    } else if (value > node->data) {
+        node->right = removeRecursive(node->right, value);
+    } 
+
+    else {
+
+        if (node->left == nullptr && node->right == nullptr) {
+            delete node;
+            std::cout << "Usunieto lisc " << value << ".\n";
+            return nullptr;
+        }
+        
+
+        
+        else if (node->left == nullptr) {
+            Node* temp = node->right;
+            delete node;
+            std::cout << "Usunieto " << value << " (mial jedno dziecko).\n";
+            return temp;
+        } else if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            std::cout << "Usunieto " << value << " (mial jedno dziecko).\n";
+            return temp;
+        }
+
+        Node* successor = findMin(node->right);
+        node->data = successor->data;
+        std::cout << "Usunieto " << value << " (mial dwoje dzieci, zastapiono przez " << successor->data << ").\n";
+        node->right = removeRecursive(node->right, successor->data);
+    }
+    return node;
+}
+
+Node* BST::findMin(Node* node) {
+    while (node != nullptr && node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
+
 #endif
